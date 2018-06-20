b44af55a80a37a3ae339a66a5f2f96076f5d66a3
maddog0223 <kamishimamadoka1@gmail.com>
2018-06-20 09:45:09 -0700

    hw

--- part_08/Exception_Practice/ExceptionClass.java
+++ part_08/Exception_Practice/ExceptionClass.java
@@ -4,15 +4,27 @@ public class ExceptionClass {
 
     public static void main(String[] args) {
 
-       try {
-           int a = 4;
 
-        int b = 0;
-        int c = a/b;
-        System.out.println(c);
-       }catch (ArithmeticException ze){
+        try {
 
-           System.out.println("Can't divide by zero");
-       }
+            try {
+                int a = 4;
+
+                int b = 0;
+                int c = a / b;
+                System.out.println(c);
+            } catch (ArithmeticException ze) {
+
+                System.out.println("Can't divide by zero");
+            }
+
+            int[] array = new int[10];
+            System.out.println(array[20]);
+
+        }catch (ArrayIndexOutOfBoundsException aro) {
+
+            System.out.println("The array does not exist");
+        }
     }
+
 }
